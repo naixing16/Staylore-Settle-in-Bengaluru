74337812ddf9e5ad838d42be2ce124a7293def2d
Shamanth K M <shamanthkolegodu@gmail.ci=om>
2020-12-07 10:43:20 +0530

    project

---
+++ houses-website/backend/routes/login.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+let Registration = require('../models/registration');
+
+router.route('/login').post((req, res) =>{
+    const Username = req.body.username;
+    const Password = req.body.password;
+    Registration.find({ username: Username})
+        .then(data=>{
+            if(data[0].password == Password){
+                res.json({st:"Successfull",data:username[0].username});
+            }
+            else{
+                res.json({st:"Unsucessfull"});
+            }
+        }).catch(err => res.json({st:"Not found!"}))});
+module.exports = router;
